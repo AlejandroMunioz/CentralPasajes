c57db1203f61824f2b50fac8cb7909fc2ff7d4ae
amunoz <ale.dgeyc@gmail.com>
2024-08-27 11:33:32 -0300

    challenge

---
+++ CentralPasajes/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Crypto;
+Console.WriteLine("introduzca el texto");
+string entrada = Console.ReadLine();
+var alg1 = new Algoritmo1();
+
+
+Func<int, int> FuncDencrypt = x => x - 1;
+Func<int, int> FuncEncrypt = x => x + 1;
+var alg2 = new Algoritmo2(FuncEncrypt, FuncDencrypt);
+var alg3 = new Algoritmo3();
+
+Encriptador enc = new Encriptador(alg1, alg2, alg3);
+var result = enc.Encrypt(entrada);
+
+Console.WriteLine($"El texto cifrado es: {result}");
+
+Console.WriteLine($"El texto descriptado es: {enc.Decrypt(result)}");
---
+++ Crypto/Algoritmos.cs
@@ -0,0 +1,99 @@
+﻿namespace Crypto
+{
+    public class Encriptador {
+        ISecurityElement _algoritmo1;
+        ISecurityElement _algoritmo2;
+        ISecurityElement _algoritmo3;
+        public Encriptador(ISecurityElement algoritmo1, ISecurityElement algoritmo2, ISecurityElement algoritmo3) {
+            _algoritmo1 = algoritmo1;
+            _algoritmo2 = algoritmo2;
+            _algoritmo3 = algoritmo3;
+        }
+
+        public string Encrypt(string text)
+        {
+            return _algoritmo3.Encrypt(_algoritmo2.Encrypt(_algoritmo1.Encrypt(text)));
+        }
+        public string Decrypt(string text) {
+            return _algoritmo1.Decrypt(_algoritmo2.Decrypt(_algoritmo3.Decrypt(text)));
+            
+        }
+    }
+    public class Algoritmo1 : ISecurityElement {
+        public  string StringReverse(string texto)
+        {
+            char[] charArray = texto.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public  string Encrypt(string text)
+        {
+            return StringReverse(text);
+        }
+        public  string Decrypt(string text)
+        {
+            return StringReverse(text);
+        }
+    }
+    public class Algoritmo2 : ISecurityElement {
+        public Func<int, int> FuncEncrypt { get; set; }
+        public Func<int, int> FuncDencrypt { get; set; }
+        public Algoritmo2(Func<int, int> funcEncrypt, Func<int, int> funcDencrypt) {
+            this.FuncEncrypt = funcEncrypt;
+            this.FuncDencrypt = funcDencrypt;
+        }
+        public string Encrypt(string text) {
+            var result = new char[text.Length];
+            for (int i = 0; i < text.Length; i++) {
+                result[i] = (char)FuncEncrypt(Convert.ToInt32(text[i]));
+            }
+            return new string(result);
+        }
+        public string Decrypt(string text) {
+            var result = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                result[i] = (char)FuncDencrypt(Convert.ToInt32(text[i]));
+            }
+            return new string(result);
+        }
+    }
+    public class Algoritmo3 : ISecurityElement {
+    
+        public  string Encrypt(string text)
+        {
+            
+
+            return changeEncrypt(text, 0, text.Length - 1);
+        }
+        public  string Decrypt(string text)
+        {
+            return changeDecrypt(text, 0 , text.Length - 1);
+        }
+        public  string changeEncrypt(string text, int start, int end) { 
+          if(start == end) return text;
+           var result = text.ToCharArray();
+           result[start] = Convert.ToInt32(text[start]) == 65535 ? text[start]  : (char)Convert.ToInt32(text[start] +1);
+         return changeEncrypt(string.Join("",result.Select(x=>x.ToString())), start+1, end);
+            
+        }
+        public  string changeDecrypt(string text, int start, int end)
+        {
+            if (start == end) return text;
+            if (start == end) return text;
+            var result = text.ToCharArray();
+            result[start] = Convert.ToInt32(text[start]) == 65535 ? text[start] : (char)Convert.ToInt32(text[start] - 1);
+            return changeDecrypt(string.Join("", result.Select(x => x.ToString())), start + 1, end);
+
+        }
+    }
+    public interface ISecurityElement
+    {
+        string Encrypt(string text) ;
+        string Decrypt(string text);
+    }
+
+    //https://stackoverflow.com/questions/5348844/how-to-convert-a-string-to-ascii
+
+}
